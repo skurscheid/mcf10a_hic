e1c72b51dfc85d2baa3b2454be9b514af218346f
Sebastian Kurscheid <genomicsio@gmail.com>
2019-06-05 18:18:01 +1000

    adds hicCorrelate rules

---
+++ scripts/scratch.py
@@ -0,0 +1,6 @@
+import pandas as pd
+wildcards = dict()
+wildcards = {"batch" : "170306_NB501086_0102_HiC1_6_run4",
+             "tool" : "hicexplorer",
+             "command" : "hicBuildMatrix_bin",
+             "sub_command" : "10000"}
