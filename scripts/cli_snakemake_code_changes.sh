a417bba5fff8e98389163c79affedd9f19308979
Sebastian Kurscheid <genomicsio@gmail.com>
2020-02-11 11:09:06 +1100

    snakemake master script modified

--- scripts/cli_snakemake_code_changes.sh
+++ scripts/cli_snakemake_code_changes.sh
@@ -1,5 +1,4 @@
-export cli_target=${1}
-/home/150/sxk150/miniconda3/envs/snakemake/bin/snakemake -s /home/150/sxk150/cellular_identity_nucleome/Snakefile ${1}\
+/home/150/sxk150/miniconda3/envs/snakemake/bin/snakemake -s /home/150/sxk150/cellular_identity_nucleome/Snakefile\
     --configfile /home/150/sxk150/cellular_identity_nucleome/config.yaml\
 	--use-conda\
 	-d /scratch/kv78/cellular_identity_nucleome\
@@ -8,5 +7,4 @@ export cli_target=${1}
 	--cluster-config /home/150/sxk150/cellular_identity_nucleome/cluster.json\
         --keep-going\
 	-pr\
-	-R `/home/150/sxk150/cellular_identity_nucleome/scripts/cli_snakemake.sh all_combine_bam_files --lc`\
-	${2}
+	-R `/home/150/sxk150/cellular_identity_nucleome/scripts/cli_snakemake.sh ${1} --lc`\
