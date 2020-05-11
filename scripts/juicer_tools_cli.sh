f14b9e9dadcc56e4bcbc18018db5c9da419dfb22
skurscheid <sebastian.kurscheid@anu.edu.au>
2020-05-11 15:03:01 +1000

    file does not belong in this branch

--- scripts/juicer_tools_cli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#java -Xms4g -Xmx128g -jar ~/software/bin/juicer/scripts/common/juicer_tools.jar pre $1 $2 /home/150/sxk150/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt
-
-#java -Xms4g -Xmx128g -jar ~/software/bin/juicer/scripts/common/juicer_tools.jar pre merged_nodups.txt.gz inter.hic /home/150/sxk150/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt -f home/150/sxk150/software/bin/juicer/restriction_sites/hg38_Arima.txt -s inter.txt -g inter_hists.m
