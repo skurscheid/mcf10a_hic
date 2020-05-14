7da2533a4d3ca6be967837f86f5e1779d2d4da09
Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
2020-05-14 12:10:59 +1000

    adds cli scripts for runnign juicer

---
+++ scripts/qsub_juicer_arima.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -P pb97
+#PBS -l walltime=48:00:00
+#PBS -l wd
+#PBS -q biodev
+#PBS -e /home/150/sxk150/qsub_error
+#PBS -o /home/150/sxk150/qsub_out
+#PBS -l ncpus=32
+#PBS -l mem=190GB
+#PBS -M skurscheid@gmail.com
+#PBS -m abe
+#PBS -l storage=scratch/kv78+gdata/kv78
+module load bwa/0.7.17
+module load java/jdk-8.40
+~/software/bin/juicer/scripts/juicer.sh -g hg38 -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt -y ~/software/bin/juicer/restriction_sites/hg38_Arima.txt -D ~/software/bin/juicer -s Arima -t 32
+
---
+++ scripts/qsub_juicer_hindiii.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P pb97
+#PBS -l walltime=48:00:00
+#PBS -l wd
+#PBS -q biodev
+#PBS -e /home/150/sxk150/qsub_error
+#PBS -o /home/150/sxk150/qsub_out
+#PBS -l ncpus=32
+#PBS -l mem=190GB
+#PBS -M skurscheid@gmail.com
+#PBS -m abe
+#PBS -l storage=scratch/kv78+gdata/kv78
+
+about=${cli_about}
+
+module load bwa/0.7.17
+module load java/jdk-8.40
+~/software/bin/juicer/scripts/juicer.sh -g hg38 -a $about -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt -y ~/software/bin/juicer/restriction_sites/hg38_HindIII.txt -D ~/software/bin/juicer -s HindIII -t 32
+
