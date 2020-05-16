a36ad732538009c5fc2865e99414dedd7ca8d719
skurscheid <sebastian.kurscheid@anu.edu.au>
2020-05-16 11:05:24 +1000

    changes PBS parameters

--- scripts/qsub_juicer_arima.sh
+++ scripts/qsub_juicer_arima.sh
@@ -5,12 +5,14 @@
 #PBS -q biodev
 #PBS -e /home/150/sxk150/qsub_error
 #PBS -o /home/150/sxk150/qsub_out
-#PBS -l ncpus=32
-#PBS -l mem=190GB
+#PBS -l ncpus=8
+#PBS -l mem=128GB
 #PBS -M skurscheid@gmail.com
 #PBS -m abe
 #PBS -l storage=scratch/kv78+gdata/kv78
+
 module load bwa/0.7.17
 module load java/jdk-8.40
+
 ~/software/bin/juicer/scripts/juicer.sh -g hg38 -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt -y ~/software/bin/juicer/restriction_sites/hg38_Arima.txt -D ~/software/bin/juicer -s Arima -t 32
 
--- scripts/qsub_juicer_mega_arima.sh
+++ scripts/qsub_juicer_mega_arima.sh
@@ -5,8 +5,8 @@
 #PBS -q biodev
 #PBS -e /home/150/sxk150/qsub_error
 #PBS -o /home/150/sxk150/qsub_out
-#PBS -l ncpus=32
-#PBS -l mem=190GB
+#PBS -l ncpus=8
+#PBS -l mem=128GB
 #PBS -M skurscheid@gmail.com
 #PBS -m abe
 #PBS -l storage=scratch/kv78+gdata/kv78
